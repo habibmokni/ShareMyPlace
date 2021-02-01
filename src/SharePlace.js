296a80b481635b226bc09ee7ceb825b9508962bd
habibmokni <habib.mokni@outlook.fr>
2021-02-01 12:50:26 +0100

    Getting User Location.
    using  : navigator.geolocation.getCurrentPosition

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -8,10 +8,25 @@ class PlaceFinder {
     }
 
     locateUserHandler() {
-
+        if (!navigator.geolocation) {
+            alert('Location feature is not available in your browser - please use a more modern browser or manually enter your address.');
+            return;
+        }
+        navigator.geolocation.getCurrentPosition(
+            successResult => {
+                const coordinates = {
+                    lat: successResult.coords.latitude,
+                    lng: successResult.coords.longitude
+                }
+                console.log(coordinates);
+            }, error => {
+                alert('Could not locate you unfortunately. Please enter an address manually.')
+            });
     }
 
     findAddressHandler() {
 
     }
 }
+
+const placeFinder = new PlaceFinder();
